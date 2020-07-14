7f93e6235d9a1499c267924e078b06c3367aa0ee
catsagan <carchimichel@gmail.com>
2020-07-14 16:33:41 -0500

    pg Conect

--- src/modelo/pgConect.java
+++ src/modelo/pgConect.java
@@ -5,10 +5,92 @@
  */
 package modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Asus
  */
 public class pgConect {
+        private final static String cadenaConexion = "jdbc:postgresql://database-1.cmcd0p4ioiqd.us-east-1.rds.amazonaws.com:5432/AWSdatabase";
+    private final static String pgusuario = "postgres";
+    private final static String pgpass = "12345678";
+    
+   
+    
+
+    private Connection con;
+    private Statement st;
+    private ResultSet rs;
+
+    
+
+    public pgConect() {
+        conectar();
+    }
+    public Connection conectar(){
+        try {
+            Class.forName("org.postgresql.Driver");
+           
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(pgConect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            con = DriverManager.getConnection(cadenaConexion, pgusuario, pgpass);
+           return con; 
+        } catch (SQLException ex) {
+            Logger.getLogger(pgConect.class.getName()).log(Level.SEVERE, null, ex);
+            return null;        
+        }
+    }
+    
+    public PreparedStatement getPs(String sql) {
+        try {
+            return con.prepareStatement(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(pgConect.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    public SQLException noquery(PreparedStatement ps) {
+        try {
+            int res = ps.executeUpdate();
+            return null;
+        } catch (SQLException ex) {
+            Logger.getLogger(pgConect.class.getName()).log(Level.SEVERE, null, ex);
+            return ex;
+        }
+    }
+
+    public SQLException noquery(String sql) {
+        try {
+            st = con.createStatement();
+            st.execute(sql);
+            st.close();
+            return null;
+        } catch (SQLException ex) {
+            Logger.getLogger(pgConect.class.getName()).log(Level.SEVERE, null, ex);
+            return ex;
+        }
+    }
+
+    public ResultSet query(String sql) {
+        try {
+            st = con.createStatement();
+            rs = st.executeQuery(sql);
+            return rs;
+        } catch (SQLException ex) {
+            Logger.getLogger(pgConect.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+
+    }
     
 }
