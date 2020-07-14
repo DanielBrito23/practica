f4f4a295be7011ce53b4f5b016a8f01ee2162a0e
Danicho23 <henry.brito4040@gmail.com>
2020-07-14 11:52:50 -0500

    test
    mensaje

--- src/practap/PracTap.java
+++ src/practap/PracTap.java
@@ -16,6 +16,7 @@ public class PracTap {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("mensaje de prueba");
     }
     
 }
